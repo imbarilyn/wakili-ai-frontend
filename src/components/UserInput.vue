4dbda289fe78cad11f441fa38151ad87635957b0
muhonja <linahmuhonjaimbari@gmail.com>
2024-07-24 17:19:32 +0300

    add UserInput component

---
+++ src/components/UserInput.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="relative border-2 border-main-color shadow-lg shadow-slate-300/10 rounded-lg flex flex-row">
+    <textarea class="w-full grow bg-transparent border-none focus:outline-none resize-none">
+
+    </textarea>
+  </div>
+
+</template>
