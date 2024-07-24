b2807b98e1901d38c2f01e5fb3f082c30a596236
muhonja <linahmuhonjaimbari@gmail.com>
2024-07-24 17:17:01 +0300

    add loading Component

---
+++ src/components/LoadingPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="absolute inset-0 cursor-wait backdrop-blur">
+    <div class="w-full h-full flex justify-center items-center">
+      <span class="loading loading-spinner loading-lg"></span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
