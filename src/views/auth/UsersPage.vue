185da0f5e1a353e76b448bd2a718ef910edaa586
muhonja <linahmuhonjaimbari@gmail.com>
2024-07-24 17:21:55 +0300

    add UserPage for Dynamic routing for user signup and login

---
+++ src/views/auth/UsersPage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+
+
+<template>
+  <div>
+    <RouterView #default="{Component, route}">
+      <template v-if="Component">
+        <component :is="Component" :key="route.fullPath"/>
+      </template>
+
+    </RouterView>
+  </div>
+
+</template>
