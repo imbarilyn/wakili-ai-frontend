ec957df8c04fc96e0aee3961537700968636ddc8
muhonja <linahmuhonjaimbari@gmail.com>
2024-07-24 17:13:44 +0300

    add Routerview

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <script setup lang="ts">
 
 import ChatPage from '@/views/chatbot/ChatPage.vue'
+import ToastAlert from '@/components/toasts/ToastAlert.vue'
+import ToastContainer from '@/components/toasts/ToastContainer.vue'
+import { useNotificationsStore } from '@/stores'
+
+const notificationsStore = useNotificationsStore()
 </script>
 
 <template>
-  <ChatPage />
+  <RouterView>
+<template #default="{ Component, route }">
+      <component :is="Component" :key="route.fullPath"/>
+</template>
+  </RouterView>
+
+  <teleport to="body">
+    <ToastContainer v-if="notificationsStore.hasNotifications">
+      <template v-for="notification in notificationsStore.getNotifications" :key="notification.id">
+        <ToastAlert
+          v-if="notification.id && notification.isShown"
+          :id="notification.id"
+          :is-shown="notification.isShown"
+          :message="notification.message"
+          :type="notification.type"
+        />
+      </template>
+    </ToastContainer>
+  </teleport>
 </template>
 
 <style scoped>
