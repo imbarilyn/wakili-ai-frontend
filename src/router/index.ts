910923973edc356c0a14a78676f93523a4a537b9
muhonja <linahmuhonjaimbari@gmail.com>
2024-08-07 17:32:54 +0300

    add navigation guards

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores'
+
 
 
 
   const routes: any =[
+    {
+      name: 'home',
+      path: '/',
+      redirect: '/chat'
+    },
     {
       name: 'auth',
       path: '/auth',
@@ -31,6 +38,11 @@ import { createRouter, createWebHistory } from 'vue-router'
       ]
     },
     {
+      name: 'chat-subscription',
+      path: '/chatId/chat-subscription',
+      component: ()=>import('../views/chatbot/SubscriptionPage.vue'),
+    },
+    {
       name: 'chat-page',
       path: '/chat',
       component: () => import('../views/chatbot/ChatPage.vue'),
@@ -39,8 +51,30 @@ import { createRouter, createWebHistory } from 'vue-router'
           name: 'new-chat',
           path: '/new-chat',
           component: () => import('../views/chatbot/NewChatPage.vue')
+        },
+        {
+          name: 'chat-history',
+          path: 'chatId/chat-history',
+          component: ()=>import('../views/chatbot/ChatHistory.vue'),
+          // props:(route: any)=>{
+          //   return {
+          //     chatId: route.params.chatId
+          //   }
+          // }
+
+        },
+      ],
+      beforeEach(to: any, from: any, next: any){
+        const authStore = useAuthStore()
+        if(authStore.getUserInfo()){
+          console.log(to.name)
+          next()
         }
-      ]
+        else{
+          next({name: 'user-login'})
+        }
+      }
+
     },
     {
       path: '/:notFound(.*)*',
@@ -55,7 +89,30 @@ const router = createRouter({
 } as any)
 
 router.beforeEach((to, _from, next)=>{
-
+  const authStore = useAuthStore()
+  const excludedRoutes = [
+    'user-forgot-password',
+    'user-reset-password',
+    'not-found',
+    'new-chat',
+    'chat',
+    'home',
+    'chat-page',
+    'sign-up'
+  ]
+  const isExcludedRoute = excludedRoutes.includes(to.name as string)
+  if(!authStore.userIsLoggedIn){
+    if(isExcludedRoute){
+      console.log("you are not logged in")
+      console.log('going to', to.name)
+      next({name: 'user-login'})
+    }
+    else{
+      console.log(to.name)
+      next()
+    }
+  }
+  next()
 })
 
 
